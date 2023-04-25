b2c75bc8b9ecfa3ad6f150f30c3e295c767b1de1
tcpassos <tcpassos99@gmail.com>
2023-04-25 00:17:02 -0300

    Updated .gitignore

--- RiffRunner/main.cpp
+++ RiffRunner/main.cpp
@@ -1,25 +1,29 @@
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    /* Cria a janela */
+    window = glfwCreateWindow(800, 600, "Riff Runner", NULL, NULL);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
+    // Define o ícone da janela
+    GLFWimage icons[1];
+    icons[0].pixels = stbi_load("resources/img/icon.png", &icons[0].width, &icons[0].height, 0, 4);
+    glfwSetWindowIcon(window, 1, icons);
+    stbi_image_free(icons[0].pixels);
+
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    /* Executa em loop até a janela ser fechada */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
